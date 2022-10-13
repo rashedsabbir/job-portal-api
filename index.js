d386b7167f99b06ba0b5eaa5c64eedf724d453ce
rashedsabbir <rashedulhasan1996@gmail.com>
2022-10-13 22:11:23 +0600

    mongoose connected

---
+++ index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const app = require("./app");
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poglh.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(uri).then(() => {
+  console.log(`Successfully connected to database`.blue.bold);
+});
+
+// server
+const port = process.env.PORT || 5000;
+const crypto = require("crypto");
+// app.listen(port, () => {
+//   console.log(`http://localhost:${port}`.yellow.bold);
+// });
