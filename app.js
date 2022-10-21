cf19d01638066d5b7032996bce3830f54c8fbfef
rashedsabbir <rashedulhasan1996@gmail.com>
2022-10-21 23:00:50 +0600

    routes added to app

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// // import routes
-// const userRoute = require("./routes/user.route");
-// const jobRoute = require("./routes/job.route");
-// const managerRoute = require("./routes/manager.route");
-// const adminRoute = require("./routes/admin.route");
+// import routes
+const userRoute = require("./routes/user.route");
+const jobRoute = require("./routes/job.route");
+const managerRoute = require("./routes/manager.route");
+const adminRoute = require("./routes/admin.route");
 
-// // routes
-// app.use("/admin", adminRoute);
-// app.use("/user", userRoute);
-// app.use("/jobs", jobRoute);
-// app.use("/manager", managerRoute);
+// routes
+app.use("/admin", adminRoute);
+app.use("/user", userRoute);
+app.use("/jobs", jobRoute);
+app.use("/manager", managerRoute);
 
 app.get("/", (req, res) => {
   res.send("Routes started");
